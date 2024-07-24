c0e90e84d88dd5be0f191ea1ce8432ffb20e249c
amitgade <amitgaade@gmail.com>
2024-07-24 20:12:53 +0530

    action-Modify Main method

--- 04-dsa/src/dev/amit/dsa/datastructures/linkedlist/Main.java
+++ 04-dsa/src/dev/amit/dsa/datastructures/linkedlist/Main.java
@@ -2,16 +2,30 @@ package dev.amit.dsa.datastructures.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+         * getHead, getTail, getLength, printList
+         * */
 //        LinkedList myLinkedList = new LinkedList(1);
 //        myLinkedList.getHead();
 //        myLinkedList.getTail();
 //        myLinkedList.getLength();
 //        myLinkedList.printList();
 
+        /*
+         * append
+         * */
+//        LinkedList myLinkedList = new LinkedList(1);
 //        myLinkedList.append(2);
 //        myLinkedList.printList();
-//        System.out.println("**************************");
+
+        /*
+        * removeLast
+        * */
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.printList();
 //
+//        System.out.println("*********** removeLast ***************");
 //        // (2) items
 //        System.out.println(myLinkedList.removeLast().value);
 //
@@ -21,17 +35,23 @@ public class Main {
 //        // (0) items
 //        System.out.println(myLinkedList.removeLast());
 
+        /*
+         * prepend
+         */
 //        LinkedList myLinkedList = new LinkedList(2);
 //        myLinkedList.append(3);
 //        myLinkedList.printList();
-//        System.out.println("***************************");
+//        System.out.println("************* prepend **************");
 //        myLinkedList.prepend(1);
 //        myLinkedList.printList();
 
+        /*
+         * RemoveFirst
+         * */
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
         myLinkedList.printList();
-        System.out.println("******* RemoveFirst *******************");
+        System.out.println("************ RemoveFirst **************");
 
         // (2) items
         System.out.println(myLinkedList.removeFirst().value);
