2127eaab35ad697398ff6fcdd32cc8cdf2881d50
amitgade <amitgaade@gmail.com>
2024-07-24 20:27:33 +0530

    action-Add code to get item based on index

--- 04-dsa/src/dev/amit/dsa/datastructures/linkedlist/Main.java
+++ 04-dsa/src/dev/amit/dsa/datastructures/linkedlist/Main.java
@@ -2,25 +2,25 @@ package dev.amit.dsa.datastructures.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-         * getHead, getTail, getLength, printList
-         * */
+//        /*
+//        getHead, getTail, getLength, printList
+//         */
 //        LinkedList myLinkedList = new LinkedList(1);
 //        myLinkedList.getHead();
 //        myLinkedList.getTail();
 //        myLinkedList.getLength();
 //        myLinkedList.printList();
 
-        /*
-         * append
-         * */
+//        /*
+//        append
+//         */
 //        LinkedList myLinkedList = new LinkedList(1);
 //        myLinkedList.append(2);
 //        myLinkedList.printList();
 
-        /*
-        * removeLast
-        * */
+//        /*
+//        removeLast
+//         */
 //        LinkedList myLinkedList = new LinkedList(1);
 //        myLinkedList.append(2);
 //        myLinkedList.printList();
@@ -35,9 +35,10 @@ public class Main {
 //        // (0) items
 //        System.out.println(myLinkedList.removeLast());
 
-        /*
-         * prepend
-         */
+
+//        /*
+//        prepend
+//         */
 //        LinkedList myLinkedList = new LinkedList(2);
 //        myLinkedList.append(3);
 //        myLinkedList.printList();
@@ -45,21 +46,35 @@ public class Main {
 //        myLinkedList.prepend(1);
 //        myLinkedList.printList();
 
+//        /*
+//        RemoveFirst
+//         */
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.printList();
+//        System.out.println("************ RemoveFirst **************");
+//
+//        // (2) items
+//        System.out.println(myLinkedList.removeFirst().value);
+//
+//        // (1) items
+//        System.out.println(myLinkedList.removeFirst().value);
+//
+//        // (0) items
+//        System.out.println(myLinkedList.removeFirst());
+
         /*
-         * RemoveFirst
-         * */
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
-        myLinkedList.printList();
-        System.out.println("************ RemoveFirst **************");
+        get(i)
+         */
+        LinkedList myLinedList = new LinkedList(0);
+        myLinedList.append(1);
+        myLinedList.append(2);
+        myLinedList.append(3);
+        myLinedList.printList();
+        System.out.println("************ get(i) **************");
+        System.out.println(myLinedList.get(2).value);
+        System.out.println("************ get(i) **************");
+        myLinedList.printList();
 
-        // (2) items
-        System.out.println(myLinkedList.removeFirst().value);
-
-        // (1) items
-        System.out.println(myLinkedList.removeFirst().value);
-
-        // (0) items
-        System.out.println(myLinkedList.removeFirst());
     }
 }
