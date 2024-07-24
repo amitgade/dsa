bcbef25ce7c9cff223b7ae6cef155969ad8d8a55
amitgade <amitgaade@gmail.com>
2024-07-24 19:44:59 +0530

    action-Add code to test prepend and append method

--- 04-dsa/src/dev/amit/dsa/datastructures/linkedlist/Main.java
+++ 04-dsa/src/dev/amit/dsa/datastructures/linkedlist/Main.java
@@ -2,23 +2,30 @@ package dev.amit.dsa.datastructures.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(1);
+//        LinkedList myLinkedList = new LinkedList(1);
 //        myLinkedList.getHead();
 //        myLinkedList.getTail();
 //        myLinkedList.getLength();
 //        myLinkedList.printList();
 
-        myLinkedList.append(2);
+//        myLinkedList.append(2);
+//        myLinkedList.printList();
+//        System.out.println("**************************");
+//
+//        // (2) items
+//        System.out.println(myLinkedList.removeLast().value);
+//
+//        // (1) items
+//        System.out.println(myLinkedList.removeLast().value);
+//
+//        // (0) items
+//        System.out.println(myLinkedList.removeLast());
+
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(3);
         myLinkedList.printList();
-        System.out.println("**************************");
-
-        // (2) items
-        System.out.println(myLinkedList.removeLast().value);
-
-        // (1) items
-        System.out.println(myLinkedList.removeLast().value);
-
-        // (0) items
-        System.out.println(myLinkedList.removeLast());
+        System.out.println("***************************");
+        myLinkedList.prepend(1);
+        myLinkedList.printList();
     }
 }
