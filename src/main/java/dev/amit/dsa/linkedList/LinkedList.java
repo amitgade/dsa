92648019f1a784eea3dd26f31f27a1ac006f7284
amitgade <amitgaade@gmail.com>
2025-12-12 20:11:16 +0530

    action-prepend implementation

--- src/main/java/dev/amit/dsa/linkedList/LinkedList.java
+++ src/main/java/dev/amit/dsa/linkedList/LinkedList.java
@@ -52,4 +52,15 @@ public class LinkedList {
         }
         length++;
     }
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
 }
--- src/main/java/dev/amit/dsa/linkedList/Main.java
+++ src/main/java/dev/amit/dsa/linkedList/Main.java
@@ -2,11 +2,11 @@ package dev.amit.dsa.linkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.append(4);
-        myLinkedList.append(5);
+        LinkedList myLinkedList = new LinkedList(5);
+        myLinkedList.prepend(4);
+        myLinkedList.prepend(3);
+        myLinkedList.prepend(2);
+        myLinkedList.prepend(1);
         myLinkedList.printList();
 
     }
