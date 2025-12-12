cf1f73bf324a5155bce0cd86faacdc91d21ac7fa
amitgade <amitgaade@gmail.com>
2025-12-12 20:27:23 +0530

    action-hasLoop implementation

--- src/main/java/dev/amit/dsa/linkedList/LinkedList.java
+++ src/main/java/dev/amit/dsa/linkedList/LinkedList.java
@@ -187,4 +187,17 @@ public class LinkedList {
         }
         return slow;
     }
+    // HASLOOP METHOD // - Floyd's Tortoise and Hare algorithm
+    public boolean hasLoop() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
