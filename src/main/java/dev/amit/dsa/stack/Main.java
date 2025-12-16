b4ce5823226c58fb94e39490e4151209fd8c3d11
amitgade <43432815+amitgade@users.noreply.github.com>
2025-12-16 22:05:15 +0530

    action-Stack pop

--- src/main/java/dev/amit/dsa/stack/Main.java
+++ src/main/java/dev/amit/dsa/stack/Main.java
@@ -2,10 +2,23 @@ package dev.amit.dsa.stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack myStack = new Stack(3);
-        myStack.push(1);
+        Stack myStack = new Stack(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(5);
+
+        myStack.printStack();
         System.out.println("Top: " + myStack.getTop().value);
         System.out.println("Height: " + myStack.getHeight());
-        myStack.printStack();
+
+        System.out.println("Pop starts");
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop());
+
     }
 }
