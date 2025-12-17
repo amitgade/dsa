ad0e090d159f5d1a1f83886d02f1f15227e12941
amitgade <43432815+amitgade@users.noreply.github.com>
2025-12-17 18:17:47 +0530

    action-Dequeue Implementation

--- src/main/java/dev/amit/dsa/queue/Main.java
+++ src/main/java/dev/amit/dsa/queue/Main.java
@@ -2,13 +2,17 @@ package dev.amit.dsa.queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue myQueue = new Queue(13);
+        Queue myQueue = new Queue(11);
         myQueue.enqueue(22);
 
-        System.out.println("First: " + myQueue.getFirst().value);
-        System.out.println("Last: " + myQueue.getLast().value);
-        System.out.println("Length: " + myQueue.getLength());
+//        System.out.println("First: " + myQueue.getFirst().value);
+//        System.out.println("Last: " + myQueue.getLast().value);
+//        System.out.println("Length: " + myQueue.getLength());
 
-        myQueue.printQueue();
+//        myQueue.printQueue();
+
+        System.out.println(myQueue.dequeue().value); // returns 11
+        System.out.println(myQueue.dequeue().value); // returns 22
+        System.out.println(myQueue.dequeue()); // returns null
     }
 }
--- src/main/java/dev/amit/dsa/queue/Queue.java
+++ src/main/java/dev/amit/dsa/queue/Queue.java
@@ -51,4 +51,18 @@ public class Queue {
         last = newNode;
         length++;
     }
+
+    public Node dequeue() {
+        if (length == 0) return null;
+        Node temp = first;
+        if (length == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
 }
